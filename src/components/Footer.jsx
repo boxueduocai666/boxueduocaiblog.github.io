020d3620c94d2f24794170d96dd8d717bf05f828
boxueduocai <boxueduocai666@gmail.com>
2026-08-25 13:22:21 +0800

    Add Footer component with copyright and text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        © 2026 boxueduocai
+      </p>
 
+      <p>
+        后来烟雨皆散尽，无人撑伞一人行
+      </p>
+    </footer>
+  )
+}
