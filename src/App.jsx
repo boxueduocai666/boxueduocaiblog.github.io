0f02b7b7bae5b2fdb59b52e6082d129416391ee6
boxueduocai <boxueduocai666@gmail.com>
2026-08-25 13:20:36 +0800

    Add routing and components to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,28 @@
+import { Routes, Route } from 'react-router-dom'
 
+import Navbar from './components/Navbar'
+import Background from './components/Background'
+
+import Home from './pages/Home'
+import Article from './pages/Article'
+import About from './pages/About'
+
+function App() {
+  return (
+    <div className="app">
+      <Background />
+
+      <Navbar />
+
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/article/:id" element={<Article />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
+export default App
