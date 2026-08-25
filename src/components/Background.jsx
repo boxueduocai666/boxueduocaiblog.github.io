0dd91ac1369a9cf80cd890e8afef243ec08a6b98
boxueduocai <boxueduocai666@gmail.com>
2026-08-25 13:20:58 +0800

    Add Background component with styled divs

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,1 +1,11 @@
+export default function Background() {
+  return (
+    <div className="background">
+      <div className="background-orb orb-one" />
+      <div className="background-orb orb-two" />
+      <div className="background-orb orb-three" />
 
+      <div className="background-noise" />
+    </div>
+  )
+}
