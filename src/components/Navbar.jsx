be69f894cb021a03b864c5c6778acb18bdc5b569
boxueduocai <boxueduocai666@gmail.com>
2026-08-29 23:02:45 +0800

    Refactor Navbar component for improved readability

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,29 @@
-import { Link, useLocation } from 'react-router-dom'
+import {
+  Link,
+  useLocation,
+} from 'react-router-dom'
+
 import LiquidCard from './LiquidCard'
 
 export default function Navbar() {
   const location = useLocation()
 
-  const isHome = location.pathname === '/'
-  const isAbout = location.pathname === '/about'
+  const isHome =
+    location.pathname === '/'
+
+  const isAbout =
+    location.pathname === '/about'
 
   return (
     <header className="navbar-wrapper">
       <LiquidCard
         className="navbar-glass"
         cornerRadius={999}
-        displacementScale={16}
-        blurAmount={0.06}
-        aberrationIntensity={0.6}
+        displacementScale={10}
+        blurAmount={0.045}
+        saturation={120}
+        aberrationIntensity={0.35}
+        elasticity={0.04}
       >
         <nav className="navbar">
           <Link
@@ -34,14 +43,22 @@ export default function Navbar() {
           <div className="navbar-links">
             <Link
               to="/"
-              className={isHome ? 'nav-active' : ''}
+              className={
+                isHome
+                  ? 'nav-active'
+                  : ''
+              }
             >
               首页
             </Link>
 
             <Link
               to="/about"
-              className={isAbout ? 'nav-active' : ''}
+              className={
+                isAbout
+                  ? 'nav-active'
+                  : ''
+              }
             >
               关于
             </Link>
