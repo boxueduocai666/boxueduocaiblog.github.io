e4d9ed1120026872428e3e72779dcfcfa9969e04
boxueduocai <boxueduocai666@gmail.com>
2026-08-29 23:02:18 +0800

    Refactor App component and update route syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
-import { Routes, Route } from 'react-router-dom'
+import {
+  Route,
+  Routes,
+} from 'react-router-dom'
 
-import Navbar from './components/Navbar'
 import Background from './components/Background'
+import Navbar from './components/Navbar'
 
 import Home from './pages/Home'
 import Article from './pages/Article'
 import About from './pages/About'
 
-function App() {
+export default function App() {
   return (
     <div className="app">
       <Background />
+
       <Navbar />
 
       <main className="main-content">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/article/:id" element={<Article />} />
-          <Route path="/about" element={<About />} />
+          <Route
+            path="/"
+            element={<Home />}
+          />
+
+          <Route
+            path="/article/:id"
+            element={<Article />}
+          />
+
+          <Route
+            path="/about"
+            element={<About />}
+          />
         </Routes>
       </main>
     </div>
   )
 }
-
-export default App
