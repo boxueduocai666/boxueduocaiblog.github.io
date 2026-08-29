558aafbf50674f02fcbb873beee24069671a0f3f
boxueduocai <boxueduocai666@gmail.com>
2026-08-29 23:00:17 +0800

    Update Background component structure and classes

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,11 +4,14 @@ export default function Background() {
       className="site-background"
       aria-hidden="true"
     >
-      <div className="ambient ambient-one" />
-      <div className="ambient ambient-two" />
-      <div className="ambient ambient-three" />
+      <div className="background-image" />
+
+      <div className="background-overlay" />
+
+      <div className="background-light" />
 
       <div className="background-vignette" />
+
       <div className="background-noise" />
     </div>
   )
